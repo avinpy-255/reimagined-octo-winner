b2791213dbf529ca95ada04facd46e67ed36b837
Avinash <avinashchowdhury81@gmail.com>
2024-06-20 12:33:09 +0530

    added curd in task

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -24,6 +24,30 @@ export const deleteTask = catchAsyncErrors(async(req,res, next)=> {
         message: "task deleted", 
     })
 })
-export const updateTask = catchAsyncErrors(async(req,res, next)=>{})
-export const getMyTask = catchAsyncErrors(async(req,res, next)=>{})
+export const updateTask = catchAsyncErrors(async(req,res, next)=>{
+    const { id } = req.params;
+    let task = await Task.findById(id);
+    if(!task){
+        return next(new ErrorHandler("task not found", 400));
+    }
+    task = await Task.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+    res.status(200).json({
+        success: true,
+        message: "task updated",
+        task,
+    })
+})
+export const getMyTask = catchAsyncErrors(async(req,res, next)=>{
+    const user = req.user._id;
+    const tasks = await Task.find({ createdBy: user});
+    res.status(200).json({
+        success: true,
+        tasks,
+        message: "tasks fetched",
+    })
+})
 export const getSingleTask = catchAsyncErrors(async(req,res, next)=>{})
